669e21aef00348779f9f191c0b279f80ebcb17fa
ryanbomo <ryanbomo@gmail.com>
2018-01-17 18:45:38 -0500

    Simplified requirements

---
+++ assignment_1/src/version1.c
@@ -0,0 +1,45 @@
+/* ------------------------------------------------------------------------- */
+/*   Copyright (C) 2017 
+                Author:  Ryan Bomalaski, rbomalaski2015@my.fit.edu
+                Florida Tech, Computer Science
+   
+       This program is free software; you can redistribute it and/or modify
+       it under the terms of the GNU Affero General Public License as published by
+       the Free Software Foundation; either the current version of the License, or
+       (at your option) any later version.
+   
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+  
+      You should have received a copy of the GNU Affero General Public License
+      along with this program; if not, write to the Free Software
+      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.              */
+/* ------------------------------------------------------------------------- */
+/* File Name: main.c
+ * Date:
+ * About:
+ * 
+ */
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include "main.h"
+#include "ssh_utils.h"
+#include "matrix_tests.h"
+
+
+int
+main (int argc, char **argv)
+{
+//print input
+  
+}
+
+
+
+
