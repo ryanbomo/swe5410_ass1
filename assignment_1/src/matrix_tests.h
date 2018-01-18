2dbbd0ddf6e4e480e40a009df188c92a9456a254
ryanbomo <ryanbomo@gmail.com>
2018-01-17 21:06:25 -0500

    removed redundant stuff

--- assignment_1/src/matrix_tests.h
+++
@@ -1,8 +0,0 @@
-//matrix_tests header file
-int isFunc(int *arr);
-int isOneToOne(int *arr);
-int isOnTo(int *arr);
-int isReflex(int *arr);
-int isSym(int *arr);
-int isTrans(int *arr);
-int partition(int *arr);
