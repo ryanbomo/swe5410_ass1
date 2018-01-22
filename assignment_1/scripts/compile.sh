7c69e38455367b7cae8eb798647891b01facc98b
ryanbomo <ryanbomo@gmail.com>
2018-01-22 17:15:16 -0500

    added sym.trans test

--- assignment_1/scripts/compile.sh
+++ assignment_1/scripts/compile.sh
@@ -6,3 +6,4 @@ gcc symmetric_test.c -o ../build/sym_test
 gcc reflexive_test.c -o ../build/ref_test
 gcc ref_sym_test.c -o ../build/ref_sym_test
 gcc transitive_test.c -o ../build/trans_test
+gcc sym_trans_test.c -o ../build/sym_trans_test
