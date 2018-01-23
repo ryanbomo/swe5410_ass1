24265b42ffae8ea26a5b8b260b4c0af61cc1a5d7
ryanbomo <ryanbomo@gmail.com>
2018-01-22 21:15:53 -0500

    updated for desired stuff

--- archive/old_src/ass_1/extras/compile_old.sh
+++ archive/old_src/ass_1/extras/compile_old.sh
@@ -5,7 +5,10 @@ cd ./src
 gcc version1.c -o ../build/version1 
 gcc symmetric_test.c -o ../build/sym_test
 gcc reflexive_test.c -o ../build/ref_test
+gcc ref_sym_test.c -o ../build/ref_sym_test
 gcc transitive_test.c -o ../build/trans_test
+gcc sym_trans_test.c -o ../build/sym_trans_test
+gcc ref_trans_test.c -o ../build/ref_trans_test
 gcc onto_test.c -o ../build/onto_test
 gcc onetoone_test.c -o ../build/onetoone_test
 gcc func_test.c -o ../build/func_test
