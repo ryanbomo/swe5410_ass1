e0bc273d18cd114c50d242f8b25d2c1545b1df9d
ryanbomo <ryanbomo@gmail.com>
2018-02-07 17:54:55 -0500

    more ass 2 work

--- assignment_2/test.sh
+++ assignment_2/test.sh
@@ -1,21 +1,14 @@
 #! /bin/bash
 
-if [ -z "$4" ] || [ "$1" == "help" ]
+if [ -z "$2" ] || [ "$1" == "help" ]
   then
     echo "Parameters are [USER] [Build Folder Path] [Modifier 1] [Modifier 2] [DEL Logs]"
     echo "[USER] - FIT Username to be SSH'd into Andrew"
     echo "[Build Folder Path] - path to build folder (don't forget last slash)"
-    echo "[Modifier 1] - Gallagher Prog modifier 1"
-    echo "[Modifier 2] - Gallagher Prog modifier 2"
-    echo "[DEL Logs] - Use value DEL to delete previous logs"
+    echo "[Modifier 1] - Gallagher Prog modifier 1 *OPTIONAL*"
+    echo "[Modifier 2] - Gallagher Prog modifier 2 *OPTIONAL*"
+    echo "[DEL Logs] - Use value DEL to delete previous logs *OPTIONAL*"
     exit 1
-fi
-
-if [ "$5" == "DEL" ]
-  then
-    echo "Delete Logs"
-    [ -e log.log ] && rm log.log
-    [ -e error.log ] && rm error.log
 fi
 
 
@@ -25,6 +18,9 @@ LOC=$2
 TESTSITE=$USER@cs-scompute.cs.fit.edu
 TESTPATH=~kgallagher/public_html/sampleprogs/
 ORACLEPATH=~kgallagher/public_html/oracles/
+DATE=`date '+%Y-%m-%d_%H:%M:%S'`
+LOGNAME=$DATE.log
+ERRORNAME=$DATE.err
 GENERATORS=(
             eq
             func
@@ -36,7 +32,7 @@ GENERATORS=(
             )
 TARGETS=( 
             eq
-	        func
+            func
             onetoone
             onto
             reflex
@@ -54,20 +50,37 @@ MYPROGS=(
             trans_test
         )
 
+if [ "$5" == "DEL" ]
+  then
+    echo "Delete Logs"
+    [ -e log.log ] && rm $LOGNAME
+    [ -e error.log ] && rm $ERRORNAME
+fi
+
 for ((II=0; II < ${#GENERATORS[@]}; ++II)) do
 
-  echo $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4 >> log.log
-  echo "" >> log.log
-  ssh $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4 >> log.log
-  echo "" >> log.log
+  echo $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4 >> $LOGNAME
+  echo "" >> $LOGNAME   #Clean Output
+  ssh $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4 >> $LOGNAME
+  echo "" >> $LOGNAME
+
   #Pipe to my program
-  ssh $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4  | /usr/bin/time --verbose $LOC${MYPROGS[II]} >> log.log 2>>error.log
-  echo "" >> log.log
-  echo "" >> error.log
+  echo "Output for Tested Prog: ${MYPROGS[II]}" >> $LOGNAME
+  echo "" >> $LOGNAME
+  echo "Time and Errors for Tested Prog: ${MYPROGS[II]}" >> $ERRORNAME
+  echo "" >> $ERRORNAME
+  ssh $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4  | /usr/bin/time --verbose $LOC${MYPROGS[II]} >> $LOGNAME 2>>$ERRORNAME
+  echo "" >> $LOGNAME
+  echo "" >> $ERRORNAME
+
   #Pipe to his Oracle
-  ssh $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4  | /usr/bin/time --verbose  ssh $TESTSITE $ORACLEPATH${TARGETS[II]} >> log.log 2>>error.log
-  echo "" >> log.log
-  echo "" >> error.log
+  echo "Output for Oracle: ${TARGETS[II]}" >> $LOGNAME
+  echo "" >> $LOGNAME
+  echo "Time and Errors for Oracle: ${TARGETS[II]}" >> $ERRORNAME
+  echo "" >> $ERRORNAME
+  ssh $TESTSITE $TESTPATH${GENERATORS[II]} $3 $4  | /usr/bin/time --verbose  ssh $TESTSITE $ORACLEPATH${TARGETS[II]} >> $LOGNAME 2>>$ERRORNAME
+  echo "" >> $LOGNAME
+  echo "" >> $ERRORNAME
 
 
 done
